3ca39ea7987a4a9078571a7dedbd87cd99f58b4c
James Gregory <james.jrg@gmail.com>
2015-01-05 12:13:14 +0000

    paket and FAKE

---
+++ Jim/build.fsx
@@ -0,0 +1,23 @@
+﻿#r "packages/Fake/tools/FakeLib.dll"
+
+open Fake   
+
+let title = "Jim"
+let authors = [ "James Gregory" ]
+let githubLink = "https://github.com/jamesjrg/JimAndCats"
+
+Target "CleanBuild" <| fun _ ->
+    CleanDir @"bin\Release"
+
+Target "Build" <| fun _ ->
+    !! "Jim.sln"
+    |> MSBuildRelease @"bin\Release" "Build"
+    |> Log "MsBuild"
+
+Target "All" DoNothing
+
+"CleanBuild" ==> "Build"
+
+"Build" ==> "All"
+
+RunTargetOrDefault "All"
