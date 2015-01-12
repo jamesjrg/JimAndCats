022ada2da152b30b0845db0e7a24a90f7ce0558d
James Gregory <james.jrg@gmail.com>
2015-01-12 15:47:57 +0000

    Fix agent message loop, and add ability to query agent for current state

--- Jim/ApplicationService.fs
+++ Jim/ApplicationService.fs
@@ -4,6 +4,10 @@ open EventPersistence
 open Jim.AppSettings
 open Jim.Domain
 
+type Message =
+    | Command of Command
+    | ListUsers of AsyncReplyChannel<User seq>
+
 type AppService () =
     let streamId = appSettings.UserStream
 
@@ -27,23 +31,29 @@ type AppService () =
 
     let save expectedVersion events = store.AppendToStream streamId expectedVersion events
 
-    let agent = MailboxProcessor.Start <| fun inbox -> 
+    let agent = MailboxProcessor<Message>.Start <| fun inbox -> 
         let rec messageLoop version state = async {
-            let! command = inbox.Receive()
-            let newEvents = handleCommand command state
-            do! save version newEvents
-            let newState = List.fold handleEvent state newEvents
-            return! messageLoop version state
+            let! message = inbox.Receive()
+
+            match message with
+            | Command command -> 
+                let newEvents = handleCommand command state
+                do! save version newEvents
+                let newState = List.fold handleEvent state newEvents
+                return! messageLoop (version + List.length newEvents) newState
+            | ListUsers replyChannel ->
+                replyChannel.Reply(state.Values)
+                return! messageLoop version state       
             }
         async {
             let! version, state = load
             return! messageLoop version state
             }
 
-    member this.createUser () = agent.Post <| CreateUser { 
+    member this.createUser () = agent.Post <| Command (CreateUser { 
             Name="Bob Holness"
             Email="bob.holness@itv.com"
             Password="p4ssw0rd"
-            }
+            })
 
-    member this.listUsers () = []
+    member this.listUsers () = agent.PostAndAsyncReply(fun replyChannel -> ListUsers (replyChannel))
