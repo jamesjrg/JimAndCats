550247c4941dfef721dfc043e14cef1ffe29e8d2
James Gregory <james.jrg@gmail.com>
2015-01-14 12:23:47 +0000

    test for bad request 400 with badly formed json

--- Jim.Tests/Json.fs
+++ Jim.Tests/Json.fs
@@ -6,6 +6,7 @@ open Suave.Http
 open Suave.Web
 open Suave.Testing
 
+open System.Net
 open System.Net.Http
 open System.Text
 
@@ -15,6 +16,9 @@ open Fuchu
 open Swensen.Unquote.Assertions
 
 let run_with' = run_with default_config
+
+let reqRespWithDefaults methd resource data mapResponse =
+    req_resp methd resource "" data None DecompressionMethods.None id mapResponse
 
 type Foo =
     { foo : string; }
@@ -27,10 +31,19 @@ let test =
     let mappingFunc (a:Foo) = 
         async {
             return { bar = a.foo }
-        }
-    let postData = new ByteArrayContent(Encoding.UTF8.GetBytes("{\"foo\":\"foo\"}"))
+        }    
 
-    let testCode () =
+    let testWithValidJson () =
+        let postData = new ByteArrayContent(Encoding.UTF8.GetBytes("{\"foo\":\"foo\"}"))
         (run_with' (mapJsonAsync mappingFunc)) |> req HttpMethod.POST "/" (Some <| postData) =? "{\"bar\":\"foo\"}"
 
-    testCase "Should map JSON from one class to another" testCode
+    let testWithInvalidJson () =
+        let postData = new ByteArrayContent(Encoding.UTF8.GetBytes("{\"foo\":foo\"}"))
+        let statusCode = (run_with' (mapJsonAsync mappingFunc)) |> reqRespWithDefaults HttpMethod.POST "/" (Some <| postData) status_code
+        statusCode =? HttpStatusCode.BadRequest
+
+    testList "Json tests"
+        [
+        testCase "Should map JSON from one class to another" testWithValidJson
+        testCase "Should return bad request" testWithValidJson
+        ]
