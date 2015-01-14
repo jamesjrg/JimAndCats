9ea46762816cb7b96daa38ba3d59065cb5f87673
James Gregory <james.jrg@gmail.com>
2015-01-14 10:13:50 +0000

    finish ES integration test, stub files for more tests

--- EventPersistence.Tests/IntegrationTests.fs
+++ EventPersistence.Tests/IntegrationTests.fs
@@ -3,6 +3,7 @@
 open EventPersistence
 
 open Xunit
+open FsUnit.Xunit
 
 type TestRecordParent =
     | Child1 of Child1
@@ -15,8 +16,15 @@ and Child2 =
     string * string
 
 [<Fact>]
-let ``Should be able to serialize and deserialize events to/from event store``() =
-    let streamId = "integrationTest"
-    let projection (event: TestRecordParent) = printfn "%A" event
-    let store = new EventPersistence.EventStore<TestRecordParent>(streamId, projection) :> IEventStore<TestRecordParent>
-    store.AppendToStream streamId -1 [Child1(2, 3); Child2("a", "b")] |> Async.RunSynchronously
+let ``Should be able to serialize and deserialize nested union events to/from event store``() =
+    async {
+        let testEvents = [Child1(2, 3); Child2("a", "b")]
+        let streamId = "integrationTest"
+        let projection (event: TestRecordParent) = printfn "%A" event
+        let store = new EventPersistence.EventStore<TestRecordParent>(streamId, projection) :> IEventStore<TestRecordParent>
+        do! store.AppendToStream streamId -1 testEvents
+        let! events, lastEvent, nextEvent = store.ReadStream streamId -1 500
+
+        events |> should equal testEvents
+    }
+    
