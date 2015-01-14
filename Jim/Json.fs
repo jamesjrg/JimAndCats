fdfc94aacf9dd04fce6eb640ff0b903b324cb421
James Gregory <james.jrg@gmail.com>
2015-01-14 09:56:11 +0000

    error handling for mapJsonAsync

--- Jim/Json.fs
+++ Jim/Json.fs
@@ -9,18 +9,32 @@ open System.Text
 (*
 Suave has its own Json module with a map_json, but it has various issues:
 - it doesn't allow for an async mapping function
-- it doesn't allow you to insert any error handling
+- it doesn't have any error handling
 - it uses DataContractJsonSerializer and hence requires you put attributes all over your DTOs
 *)
 
-//TODO: error handling for parse_post_data and Json.Net serialize/deserialize
-let mapJsonAsync (f: 'a -> Async<'b>) : Types.WebPart =
-    ParsingAndControl.parse_post_data >>=
+let mapJsonAsyncAfterParsing (f: 'a -> Async<'b>): Types.WebPart =
     fun httpContext ->
         async {
             let bytesAsString = Encoding.UTF8.GetString(httpContext.request.raw_form);
-            let requestJson = JsonConvert.DeserializeObject<'a>(bytesAsString);
-            let! response = f requestJson
-            let responseJson = JsonConvert.SerializeObject(response)
-            return! ((Successful.OK responseJson) >>= (Writers.set_mime_type "application/json")) httpContext
+
+            let maybeRequestJson = 
+                try
+                    Some (JsonConvert.DeserializeObject<'a>(bytesAsString))
+                with
+                | e -> None
+
+            match maybeRequestJson with
+            | Some requestJson ->
+                let! response = f requestJson
+                let responseJson = JsonConvert.SerializeObject(response)
+                return! ((Successful.OK responseJson) >>= (Writers.set_mime_type "application/json")) httpContext
+            | None -> return! RequestErrors.BAD_REQUEST ("Could not parse JSON: " + bytesAsString) httpContext
         }
+
+let mapJsonAsync (f: 'a -> Async<'b>) : Types.WebPart =
+    choose
+        [
+            ParsingAndControl.parse_post_data >>= mapJsonAsyncAfterParsing f
+            RequestErrors.BAD_REQUEST "Unable to parse post data"
+        ]
