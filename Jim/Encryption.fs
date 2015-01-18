b81914e3b1a7946aae2528d416fc58c37c74cc95
James Gregory <james.jrg@gmail.com>
2015-01-18 16:48:44 +0000

    lots of PBKDF2 iterations

--- Jim/Encryption.fs
+++ Jim/Encryption.fs
@@ -6,7 +6,7 @@ open System.Security.Cryptography
 
 let SALT_BYTE_SIZE = 24;
 let HASH_BYTE_SIZE = 20; // to match the size of the PBKDF2-HMAC-SHA-1 hash
-let PBKDF2_ITERATIONS = 1000;
+let PBKDF2_ITERATIONS = 128000; // OWASP recommendation 2014
 let ITERATION_INDEX = 0;
 let SALT_INDEX = 1;
 let PBKDF2_INDEX = 2;
