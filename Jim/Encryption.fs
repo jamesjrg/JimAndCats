dd0a67b7b00f8cfdf9b53ec1fff811f03238887d
James Gregory <james.jrg@gmail.com>
2015-01-18 17:08:00 +0000

    don't need such a large salt

--- Jim/Encryption.fs
+++ Jim/Encryption.fs
@@ -4,7 +4,7 @@ open System.Security.Cryptography
 
 (* based on https://cmatskas.com/-net-password-hashing-using-pbkdf2/ *)
 
-let SALT_BYTE_SIZE = 24;
+let SALT_BYTE_SIZE = 16;
 let HASH_BYTE_SIZE = 20; // to match the size of the PBKDF2-HMAC-SHA-1 hash
 let PBKDF2_ITERATIONS = 128000; // OWASP recommendation 2014
 let ITERATION_INDEX = 0;
