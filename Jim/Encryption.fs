15b3ef8d8bf566a41f5fb89bc179b95450e249b2
James Gregory <james.jrg@gmail.com>
2015-01-18 17:21:41 +0000

    comment on salt size

--- Jim/Encryption.fs
+++ Jim/Encryption.fs
@@ -5,7 +5,7 @@ open System.Security.Cryptography
 
 (* based on https://cmatskas.com/-net-password-hashing-using-pbkdf2/ *)
 
-let SALT_BYTE_SIZE = 16
+let SALT_BYTE_SIZE = 16 //http://security.stackexchange.com/questions/17994/with-pbkdf2-what-is-an-optimal-hash-size-in-bytes-what-about-the-size-of-the-s recommends 128 bit hash
 let HASH_BYTE_SIZE = 20 // to match the size of the PBKDF2-HMAC-SHA-1 hash
 let PBKDF2_ITERATIONS = 128000 // OWASP recommendation 2014
 let ITERATION_INDEX = 0
