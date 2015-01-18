c1c787ea131d0c04556a916e0547ee8b208583a5
James Gregory <james.jrg@gmail.com>
2015-01-18 17:20:30 +0000

    start work on crypto code

--- Jim/Encryption.fs
+++ Jim/Encryption.fs
@@ -1,15 +1,27 @@
 ﻿module Jim.Encryption
 
+open System
 open System.Security.Cryptography
 
 (* based on https://cmatskas.com/-net-password-hashing-using-pbkdf2/ *)
 
-let SALT_BYTE_SIZE = 16;
-let HASH_BYTE_SIZE = 20; // to match the size of the PBKDF2-HMAC-SHA-1 hash
-let PBKDF2_ITERATIONS = 128000; // OWASP recommendation 2014
-let ITERATION_INDEX = 0;
-let SALT_INDEX = 1;
-let PBKDF2_INDEX = 2;
+let SALT_BYTE_SIZE = 16
+let HASH_BYTE_SIZE = 20 // to match the size of the PBKDF2-HMAC-SHA-1 hash
+let PBKDF2_ITERATIONS = 128000 // OWASP recommendation 2014
+let ITERATION_INDEX = 0
+let SALT_INDEX = 1
+let PBKDF2_INDEX = 2
+
+let PBKDF2 password (salt: Byte array) iterations outputBytes =
+    let pbkdf2 = new Rfc2898DeriveBytes(password, salt, IterationCount = iterations)
+    pbkdf2.GetBytes(outputBytes)
 
 let PBKDF2Hash password =
-    password
+    let cryptoProvider = new RNGCryptoServiceProvider()
+    let salt = Array.zeroCreate SALT_BYTE_SIZE
+    cryptoProvider.GetBytes(salt)
+
+    let hash = PBKDF2 password salt PBKDF2_ITERATIONS HASH_BYTE_SIZE
+    PBKDF2_ITERATIONS.ToString() + ":" +
+        Convert.ToBase64String(salt) + ":" +
+        Convert.ToBase64String(hash)
