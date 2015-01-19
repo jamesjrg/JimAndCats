11da7824d729db70f89125a2a73b3c5f724eedc6
James Gregory <james.jrg@gmail.com>
2015-01-19 20:42:57 +0000

    hashing

--- Jim.Tests/Hashing.fs
+++ Jim.Tests/Hashing.fs
@@ -1,2 +1,22 @@
 ﻿module Hashing
 
+open Jim.Hashing
+
+open Fuchu
+open Swensen.Unquote.Assertions
+
+[<Tests>]
+let tests =
+    testList "Hashing tests"
+        [
+            testCase "Should be able to match a hashed password" (fun () ->            
+                let password = "sxjdfls312w3w"
+                let hash = PBKDF2Hash password
+                validatePassword password hash =? true)
+
+            testCase "Should not be able to match a hashed password with a different string" (fun () ->            
+                let password = "sxjdfls312w3w"
+                let incorrectPassword = "sxjdfls312w3"
+                let hash = PBKDF2Hash password
+                validatePassword incorrectPassword hash =? false)
+        ]
--- Jim/Hashing.fs
+++ Jim/Hashing.fs
@@ -3,7 +3,9 @@
 open System
 open System.Security.Cryptography
 
-(* based on https://cmatskas.com/-net-password-hashing-using-pbkdf2/ *)
+(* based on:
+https://cmatskas.com/-net-password-hashing-using-pbkdf2/
+*)
 
 let SALT_BYTE_SIZE = 16 //http://security.stackexchange.com/questions/17994/with-pbkdf2-what-is-an-optimal-hash-size-in-bytes-what-about-the-size-of-the-s recommends 128 bit hash
 let HASH_BYTE_SIZE = 20 // to match the size of the PBKDF2-HMAC-SHA-1 hash
@@ -12,7 +14,7 @@ let ITERATION_INDEX = 0
 let SALT_INDEX = 1
 let PBKDF2_INDEX = 2
 
-let PBKDF2 password (salt: Byte array) iterations outputBytes =
+let private PBKDF2 password (salt: Byte array) iterations outputBytes =
     let pbkdf2 = new Rfc2898DeriveBytes(password, salt, IterationCount = iterations)
     pbkdf2.GetBytes(outputBytes)
 
@@ -25,3 +27,22 @@ let PBKDF2Hash password =
     PBKDF2_ITERATIONS.ToString() + ":" +
         Convert.ToBase64String(salt) + ":" +
         Convert.ToBase64String(hash)
+
+let private slowEquals (a:byte[]) (b:byte[]) =
+    let mutable diff = (uint32 a.Length) ^^^ (uint32 b.Length);
+
+    let len = (if a.Length < b.Length then a.Length else b.Length) - 1
+    for i in 0 .. len do
+        diff <- diff ^^^ (uint32 a.[i]) ^^^ (uint32 b.[i])
+
+    diff = 0u
+
+let validatePassword password (expectedHash:string) =
+    let split = expectedHash.Split ':'
+    let iterations = Int32.Parse(split.[ITERATION_INDEX]);
+    let salt = Convert.FromBase64String(split.[SALT_INDEX]);
+    let hash = Convert.FromBase64String(split.[PBKDF2_INDEX]);
+
+    let testHash = PBKDF2 password salt iterations hash.Length
+
+    slowEquals hash testHash
