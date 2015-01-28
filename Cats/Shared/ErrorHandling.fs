4ad48a261f47c7dbd5bb06970e72564f6124c786
James Gregory <james.jrg@gmail.com>
2015-01-28 09:05:22 +0000

    reorganise stub CATS as well

---
+++ Cats/Shared/ErrorHandling.fs
@@ -0,0 +1,13 @@
+﻿module Jim.ErrorHandling
+
+type Result<'a, 'b> = 
+    | Success of 'a
+    | Failure of 'b
+
+let bind func result = 
+    match result with
+    | Success s -> func s
+    | Failure f -> Failure f
+
+let (>>=) twoTrackInput switchFunction = 
+    bind switchFunction twoTrackInput
