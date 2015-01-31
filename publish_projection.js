f8c34be0bda099b493c0a5055a700fbc741dbdd4
James Gregory <james.jrg@gmail.com>
2015-01-31 22:52:35 +0000

    work on config for sharing events

--- publish_projection.js
+++ publish_projection.js
@@ -2,16 +2,25 @@ Name: users-public
 Mode: Continuous
 Emits Enabled:true
 
-fromStream('identity').
+fromStream('jim-users-private').
     when({
         "UserCreated" : function(state,event) {
-          emit("identity-public", event.eventType, {name : event.body.value.Item.Name.Item});
+          emit("jim-users-public", event.eventType, {
+            id: event.body.value.Item.Id,
+            name : event.body.value.Item.Name.Item,
+            email : event.body.value.Item.Email.Item
+            creationTime : event.body.value.Item.CreationTime.Ticks
+          });
          },
          "NameChanged" : function(state,event) {
-          emit("identity-public", event.eventType, {name : event.body.value.Item.Name.Item});
+          emit("jim-users-public", event.eventType, {
+            id: event.body.value.Item.Id,
+            name : event.body.value.Item.Name.Item});
          },
          "EmailChanged" : function(state,event) {
-          emit("identity-public", event.eventType, {name : event.body.value.Item.Email.Item});
+          emit("jim-users-public", event.eventType, {
+            id: event.body.value.Item.Id,
+            name : event.body.value.Item.Email.Item});
          }
     });
 
