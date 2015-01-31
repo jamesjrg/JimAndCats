ac56b242303ab9f2facc46a839ee669cce9fc542
James Gregory <james.jrg@gmail.com>
2015-01-31 19:19:34 +0000

    fix warning

--- Suave.Extensions/Guids.fs
+++ Suave.Extensions/Guids.fs
@@ -6,7 +6,7 @@ open Suave.Http
 open Suave.Web
 
 //TODO: don't use exceptions
-let url_scan_guid (pf:PrintfFormat<'a,'b,'c,'d,'t>) (f:Guid -> WebPart) : WebPart =
+let url_scan_guid (pf:PrintfFormat<'a,'b,'c,'d,string>) (f:Guid -> WebPart) : WebPart =
     Applicatives.url_scan pf (fun idString ->
         match Guid.TryParse(idString) with
         | true, guid -> f guid
