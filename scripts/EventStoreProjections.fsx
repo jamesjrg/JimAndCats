aeebd074b5041c419c413def0a4b1f3a171b7a3e
James Gregory <james.jrg@gmail.com>
2015-02-02 16:58:29 +0000

    debug logging in projection creation

--- scripts/EventStoreProjections.fsx
+++ scripts/EventStoreProjections.fsx
@@ -7,11 +7,15 @@ open RestSharp
 
 let postProjection name projection =
     let host = "http://localhost:2113"
-    let client = new RestClient(host)
+    let mutable client = new RestClient(host)
+    client.Authenticator <- new HttpBasicAuthenticator("admin","changeit");
+
     let resource = sprintf "/projections/continuous?name=%s&emit=yes&checkpoints=yes&enabled=yes" name
 
     let mutable request = new RestRequest(resource, Method.POST)
     request.RequestFormat <- DataFormat.Json
     request.AddParameter(@"application\json", projection, ParameterType.RequestBody) |> ignore 
     printfn "Posting to: %s%s" host resource
-    client.Execute(request);
+    let response = client.Execute(request);
+    printfn "Status code: %A" response.StatusCode
+    printfn "Body: %s" response.Content
