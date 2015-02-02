b6487b311174bfc24567802e3e5eda84a91c18cc
James Gregory <james.jrg@gmail.com>
2015-02-02 17:07:35 +0000

    no longer need to manually parse post data in tryMapJson

--- Suave.Extensions/Json.fs
+++ Suave.Extensions/Json.fs
@@ -3,7 +3,6 @@
 open Newtonsoft.Json
 open Suave
 open Suave.Http
-open Suave.Web
 open System.Text
 
 let private tryParseJson (r:Types.HttpRequest) =
@@ -13,13 +12,6 @@ let private tryParseJson (r:Types.HttpRequest) =
   with
   | e -> None, str
 
-let private tryMapJson' (f: 'a -> Types.WebPart): Types.WebPart =
-    Suave.Types.request (fun r ->  
-      match tryParseJson r with
-      | Some request_json, _ -> f request_json
-      | None, str -> RequestErrors.BAD_REQUEST ("Could not parse JSON: " + str)
-  )
-
 let private serializeObject obj =
     JsonConvert.SerializeObject(obj)
 
@@ -28,8 +20,8 @@ let jsonOK obj = jsonResponse Successful.OK obj
 let genericNotFound = RequestErrors.NOT_FOUND "Not found"  
 
 let tryMapJson (f: 'a -> Types.WebPart): Types.WebPart =
-  choose
-    [
-      ParsingAndControl.parse_post_data >>= tryMapJson' f
-      RequestErrors.BAD_REQUEST "Unable to parse post data"
-    ]
+    Suave.Types.request (fun r ->  
+      match tryParseJson r with
+      | Some request_json, _ -> f request_json
+      | None, str -> RequestErrors.BAD_REQUEST ("Could not parse JSON: " + str)
+  )
