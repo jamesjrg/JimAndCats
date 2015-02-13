fa90f44594f103ed165045817356b8b328413d7c
James Gregory <james.jrg@gmail.com>
2015-02-13 14:43:56 +0000

    Cats.ReadModelUpdater now compiles

--- Cats.ReadModelUpdater/Program.fs
+++ Cats.ReadModelUpdater/Program.fs
@@ -2,18 +2,41 @@
 
 open Cats.ReadModelUpdater.AppSettings
 open MicroCQRS.Common
+open NodaTime
+open System
+
+type PublicIdentityEvent =
+    | UserCreated of string
+    | NameChanged of string
+    | EmailChanged of string
+
+type PrivateCatsEvent =
+    | CatCreated of CatCreated
+    | TitleChanged of TitleChanged
+
+and CatCreated = {
+    Id: Guid
+    Title: string
+    Owner: Guid
+    CreationTime: Instant
+}
+
+and TitleChanged = {
+    Id: Guid
+    Title: string
+}
 
 module private JimModel =
     let subscribe() =
         let handleUserEvent = (fun e -> ())
-        let store = new EventStore<Event>(appSettings.IdentityEventStoreIp, appSettings.IdentityEventStorePort) :> IEventStore<Event>
+        let store = new EventStore<PublicIdentityEvent>(appSettings.IdentityEventStoreIp, appSettings.IdentityEventStorePort) :> IEventStore<PublicIdentityEvent>
         store.SubscribeToStreamFrom appSettings.PublicIdentityStream 0 handleUserEvent
 
 module private CatModel =
     let subscribe() =
-        let handleUserEvent = (fun e -> ())
-        let store = new EventStore<Event>(appSettings.PrivateEventStoreIp, appSettings.PrivateEventStorePort) :> IEventStore<Event>
-        store.SubscribeToStreamFrom appSettings.PrivateCatStream 0 handleUserEvent
+        let handleCatEvent = (fun e -> ())
+        let store = new EventStore<PrivateCatsEvent>(appSettings.PrivateEventStoreIp, appSettings.PrivateEventStorePort) :> IEventStore<PrivateCatsEvent>
+        store.SubscribeToStreamFrom appSettings.PrivateCatStream 0 handleCatEvent
 
 [<EntryPoint>]
 let main argv = 
