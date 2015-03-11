4a92d67270cf5c5489ad8ee4400b736dd9a19b48
James Gregory <james.jrg@gmail.com>
2015-03-11 09:57:01 +0000

    Add async support to result builder

--- GenericErrorHandling/Result.fs
+++ GenericErrorHandling/Result.fs
@@ -6,17 +6,25 @@ module Result =
         | Success of 'a
         | Failure of 'b
 
-    let bind func result = 
-        match result with
-        | Success s -> func s
-        | Failure f -> Failure f
-
     type ResultBuilder() =
-        member this.Bind(x, f) =
-            let result = x 
-            bind f result
+        member __.Bind(result, func) =
+            match result with
+            | Success s -> func s
+            | Failure f -> Failure f |> async.Return            
 
-        member this.Return(x) = 
-            x            
+        member this.Bind(result, func) =
+            async {
+                let! result = result
+                return! this.Bind(result, func)
+            }
+
+        member __.Return data =
+            data |> Success |> async.Return
+
+        member this.ReturnFrom(result:Result<_,_>) =
+            this.Bind(result, Success >> async.Return)
+
+        member this.ReturnFrom(result:Result<_,_> Async) =
+            this.Bind(result, Success >> async.Return)
 
     let resultBuilder = new ResultBuilder()
--- Jim.CommandHandler/Domain/CommandsAndEvents.fs
+++ Jim.CommandHandler/Domain/CommandsAndEvents.fs
@@ -96,44 +96,26 @@ module private CommandHandlers =
                 | None -> Success email        
         }
 
-    let tryCreateUserAfterEmailValidation
-        (uniqueEmail: EmailAddress)
-        (createGuid: unit -> Guid)
-        (createTimestamp: unit -> Instant)
-        hashFunc
-        (command : CreateUser) =
-        //don't really need to use a custom computation builder here
-        resultBuilder {
-            let! name = createUsername command.Name
-            
-            //password hashing expensive so should come last
-            let! hash = createPasswordHash hashFunc command.Password
-
-            return Success (UserCreated {
-                Id = createGuid()
-                Name = name
-                Email = uniqueEmail
-                PasswordHash = hash
-                CreationTime = createTimestamp()
-            })
-        }
-
-    //FIXME there has to be a better way of doing this, if this was C# I could just use async combined with early returns
     let createUser
         (repository:IUserRepository)
         (createGuid: unit -> Guid)
         (createTimestamp: unit -> Instant)
         hashFunc
         (command : CreateUser) =
-        async { 
-            let maybeEmail = createEmailAddress command.Email
-            match maybeEmail with
-            | Success email -> 
-                let! uniqueEmailResult = checkForDuplicateEmail repository email
-                match uniqueEmailResult with
-                | Success uniqueEmail -> return tryCreateUserAfterEmailValidation uniqueEmail createGuid createTimestamp hashFunc command
-                | Failure f -> return Failure f
-            | Failure f -> return Failure f
+        resultBuilder { 
+            let! email = async { return createEmailAddress command.Email }
+            let! uniqueEmail = checkForDuplicateEmail repository email
+            let! name = createUsername command.Name            
+            //password hashing expensive so should come last
+            let! hash = createPasswordHash hashFunc command.Password
+
+            return! Success (UserCreated {
+                Id = createGuid()
+                Name = name
+                Email = uniqueEmail
+                PasswordHash = hash
+                CreationTime = createTimestamp()
+            })
         }
 
     let runCommandIfUserExists (repository :IUserRepository) id command f =
