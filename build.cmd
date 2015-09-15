d51d58657b63feb823104f41adb9a7a2123c28b7
James Gregory <james.gregory@bedegaming.com>
2015-09-15 14:01:08 +0100

    build script restores packages, not installs

--- build.cmd
+++ build.cmd
@@ -1,9 +1,13 @@
 @echo off
 
 if not exist .paket\paket.exe (
-    .paket\paket.bootstrapper.exe
+  .paket\paket.bootstrapper.exe
 )
-.paket\paket.exe install
+
+.paket\paket.exe restore
+if errorlevel 1 (
+  exit /b %errorlevel%
+)
 
 set encoding=utf-8
 packages\FAKE\tools\FAKE.exe build.fsx %*
